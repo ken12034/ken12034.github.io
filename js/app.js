6eece907a0a00e6581e2cae31763761e9bdbe35a
ken <ken12034@gmail.com>
2016-12-13 14:25:08 +0800

    修正1

--- js/app.js
+++ js/app.js
@@ -2,20 +2,20 @@ angular
   .module('app', [
     'ui.router'
   ])
-  .config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider) {
-    /*$urlRouterProvider.otherwise('/home');*/
-    $stateProvider
-      .state('home', {
-        url: '/',
-        views:{
-          "viewLeft" : {templateUrl: 'templates/left.html'},
-          "viewRight" : {templateUrl: 'templates/right.html'}
-        }
-        /*templateUrl: 'templates/home.html',
-        controller: 'homeCtrl'*/
-      })
-      .state('about', {
-        url: '/about',
-        templateUrl: 'templates/about.html'
-      })
-  }])
+  .config(  function($urlRouterProvider, $stateProvider , $locationProvider ) {
+      $urlRouterProvider.otherwise('/angular/');
+      $stateProvider
+        .state('home', {
+          url: '/angular/',
+          views:{
+            "viewLeft" : {templateUrl: 'angular/templates/left.html'},
+            "viewRight" : {templateUrl: 'angular/templates/right.html'},
+            "viewBottom" : {templateUrl: 'angular/templates/bottom.html'}
+          }
+        })
+        .state('about', {
+          url: '/angular/about',
+          templateUrl: 'angular/templates/about.html'
+        })
+     $locationProvider.html5Mode(true);
+  })
